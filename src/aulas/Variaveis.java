59f27d2def7e7ff03fde8ac8a15fefcb3a821227
ricardo <ricardosousaa124@gmail.com>
2026-03-30 20:03:27 -0300

    Cada Váriavel dessa é um espaço na memoria :)

---
+++ src/aulas/Variaveis.java
@@ -0,0 +1,17 @@
+package aulas;
+
+public class Variaveis {
+
+    static void main() {
+
+        String name = "Ricardo";
+        int years = 24;
+        boolean work = true;
+        double height = 1.84;
+
+        System.out.println("My name is " + name + " I have " + years + " I work " + work + " and I have " + height + " height");
+
+    }
+
+
+}
