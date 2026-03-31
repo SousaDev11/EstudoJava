8480a6b14c36ad4398ff61b332fcee3d759d2c61
ricardo <ricardosousaa124@gmail.com>
2026-03-30 21:04:19 -0300

    Entendo mais sobre variaveis, e mais uma vez dando commit, desde ja deixando minha evolução anotado nessa linja do tempo haha

--- src/aulas/Variaveis.java
+++ src/aulas/Variaveis.java
@@ -9,7 +9,10 @@ public class Variaveis {
         boolean work = true;
         double height = 1.84;
 
-        System.out.println("My name is " + name + " I have " + years + " I work " + work + " and I have " + height + " height");
+        System.out.println("Name: " + name);
+        System.out.println("Years: " + years);
+        System.out.println("Employee: " + work);
+        System.out.println("Height: " + height);
 
     }
 
