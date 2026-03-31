d745eb1c7167437d1a723d63b94958c116c51009
ricardo <ricardosousaa124@gmail.com>
2026-03-30 22:17:52 -0300

    Estudando e praticando estrutura condicional, "se" "se nao, se" "se nao"

---
+++ src/aulas/Condicionais.java
@@ -0,0 +1,19 @@
+package aulas;
+
+public class Condicionais {
+    static void main() {
+
+        int salario = 2000;
+
+        if (salario > 5000) {
+            System.out.println("Salario muito alto");
+
+        } else if (salario > 3000) {
+            System.out.println("Salario alto");
+
+        }else {
+            System.out.println("Salario baixo");
+        }
+
+    }
+}
