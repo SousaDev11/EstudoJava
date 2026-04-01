546211fbd695b7466c927ff5400fe05047cf1060
SousaDev11 <iderlenesousa775@gmail.com>
2026-04-01 19:31:03 -0300

    loop for, entendi que em vez de eu por um println em cada variavel funcionario, eu posso utiliza o FOR. (01/04/2026)

---
+++ src/aulas/LoopFor.java
@@ -0,0 +1,11 @@
+package aulas;
+
+public class LoopFor {
+
+    static void main() {
+
+        for (int funcionario = 1; funcionario <= 100 ; funcionario++) {
+            System.out.println("Funcionario " + funcionario);
+        }
+    }
+}
