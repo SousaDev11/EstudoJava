94a5777a16c70d108e186971805bcc2097a0cceb
SousaDev11 <iderlenesousa775@gmail.com>
2026-05-28 21:36:35 -0300

    praticando encapsulamento.

---
+++ src/Entity/Car.java
@@ -0,0 +1,25 @@
+package Entity;
+
+public class Car {
+
+    private String marca;
+    private int velocidade;
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+
+
+    }
+}
